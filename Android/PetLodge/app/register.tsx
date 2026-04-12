5ff7e59f29431ee6b07de2ebbdb5fc6aa441267a
Bryan <b.fernandez@estudiantec.cr>
2026-04-11 22:24:00 -0600

    Validación registro

--- Android/PetLodge/app/register.tsx
+++ Android/PetLodge/app/register.tsx
@@ -1,31 +1,89 @@
-import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Image } from "expo-image";
+import { Platform, StyleSheet } from "react-native";
 
-import { HelloWave } from '@/components/hello-wave';
-import ParallaxScrollView from '@/components/parallax-scroll-view';
-import { ThemedText } from '@/components/themed-text';
-import { ThemedView } from '@/components/themed-view';
-import { View, Text, TextInput, Pressable } from 'react-native';
-import { useState } from 'react';
+import { HelloWave } from "@/components/hello-wave";
+import ParallaxScrollView from "@/components/parallax-scroll-view";
+import { ThemedText } from "@/components/themed-text";
+import { ThemedView } from "@/components/themed-view";
+import { View, Text, TextInput, Pressable } from "react-native";
+import { Alert } from "react-native";
+import { useState } from "react";
 
 //Navegación
-import { router } from 'expo-router';
-import { Link } from 'expo-router';
+import { router } from "expo-router";
+import { Link } from "expo-router";
 
+const noNulos = (texto: string): boolean => {
+  return texto.trim() !== "";
+};
 
+const soloLetras = (texto: string, nombreCampo: string): boolean => {
+  if (/^[A-Za-z]*$/.test(texto)) {
+    return true;
+  } else {
+    Alert.alert("Error", "Solo se permiten letras en " + nombreCampo);
+    return false;
+  }
+};
+const soloNumeros = (numero: string, nombreCampo: string): boolean => {
+  if (/^[0-9]*$/.test(numero)) {
+    return true;
+  } else {
+    Alert.alert("Error", "Solo se permiten números en " + nombreCampo);
+    return false;
+  }
+};
+const numerosYletras = (texto: string, nombreCampo: string): boolean => {
+  if (/^[A-Za-z0-9]*$/.test(texto)) {
+    return true;
+  } else {
+    Alert.alert(
+      "Error",
+      "No se permiten caracteres especiales en " + nombreCampo
+    );
+    return false;
+  }
+};
 
 export default function HomeScreen() {
-  
-  const [nombre, setnombre] = useState('');
-  const [apellido, setapellido] = useState('');
-  const [cedula, setcedula] = useState('');
-  const [email, setemail] = useState('');
-  const [telefono, settelefono] = useState('');
-  const [direccion, setdireccion] = useState('');
-  const [password, setpassword] = useState('');
-  const [confirmPassword, setconfirmPassword] = useState('');
+  const [nombre, setnombre] = useState("");
+  const [apellido, setapellido] = useState("");
+  const [cedula, setcedula] = useState<number>(0);
+  const [email, setemail] = useState("");
+  const [telefono, settelefono] = useState<number>(0);
+  const [direccion, setdireccion] = useState("");
+  const [password, setpassword] = useState("");
+  const [confirmPassword, setconfirmPassword] = useState("");
 
-  
+  const handleConfirmar = () => {
+    if (!noNulos(nombre)) {
+      Alert.alert("Error", "No se permiten nulos en nombre");
+      return;
+    } else if (!noNulos(apellido)) {
+      Alert.alert("Error", "No se permiten nulos en apellido");
+      return;
+    } else if (!noNulos(cedula.toString())) {
+      Alert.alert("Error", "cedula");
+      return;
+    } else if (!noNulos(email)) {
+      Alert.alert("Error", "No se permiten nulos en email");
+      return;
+    } else if (!noNulos(telefono.toString())) {
+      Alert.alert("Error", "No se permiten nulos en teléfono");
+      return;
+    } else if (!noNulos(direccion)) {
+      Alert.alert("Error", "No se permiten nulos en dirección");
+      return;
+    } else if (!noNulos(password)) {
+      Alert.alert("Error", "No se permiten nulos en contraseña");
+      return;
+    } else if (!noNulos(confirmPassword)) {
+      Alert.alert("Error", "No se permiten nulos en contraseña");
+      return;
+    }
+    console.log("entro");
+  };
+
   return (
     <View style={styles.container}>
       <Text style={styles.titleContainer}>Registrate</Text>
@@ -34,19 +92,32 @@ export default function HomeScreen() {
         style={styles.input}
         placeholder="Nombre"
         value={nombre}
-        onChangeText={setnombre}
+        onChangeText={(text) => {
+          if (soloLetras(text, "nombre")) {
+            setnombre(text);
+          }
+        }}
       />
       <TextInput
         style={styles.input}
         placeholder="Apellido"
         value={apellido}
-        onChangeText={setapellido}
+        onChangeText={(text) => {
+          if (soloLetras(text, "apellido")) {
+            setapellido(text);
+          }
+        }}
       />
       <TextInput
         style={styles.input}
         placeholder="Cédula"
-        value={cedula}
-        onChangeText={setcedula}        
+        value={cedula.toString()}
+        keyboardType="numeric"
+        onChangeText={(text) => {
+          if (soloNumeros(text, "cédula")) {
+            setcedula(Number(text));
+          }
+        }}
       />
       <TextInput
         style={styles.input}
@@ -57,17 +128,27 @@ export default function HomeScreen() {
       <TextInput
         style={styles.input}
         placeholder="Teléfono"
-        value={telefono}
-        onChangeText={settelefono}
+        value={telefono.toString()}
+        keyboardType="numeric"
+        onChangeText={(text) => {
+          if (soloNumeros(text, "teléfono")) {
+            settelefono(Number(text));
+          }
+        }}
       />
       <TextInput
         style={styles.input}
         placeholder="Dirección"
         value={direccion}
-        onChangeText={setdireccion}
+        onChangeText={(text) => {
+          if (numerosYletras(text, "dirección")) {
+            setdireccion(text);
+          }
+        }}
       />
       <TextInput
         style={styles.input}
+        secureTextEntry
         placeholder="Contraseña"
         value={password}
         onChangeText={setpassword}
@@ -76,71 +157,68 @@ export default function HomeScreen() {
         style={styles.input}
         placeholder="Confirmar contraseña"
         value={confirmPassword}
+        secureTextEntry
         onChangeText={setconfirmPassword}
       />
 
-      <Pressable style={styles.button} onPress={null}>
+      <Pressable style={styles.button} onPress={handleConfirmar}>
         <Text style={styles.textButton}>Confirmar</Text>
       </Pressable>
 
+      <Text style={styles.text}>¿Ya tienes cuenta?</Text>
 
-          <Text style={styles.text}>¿Ya tienes cuenta?</Text>
-    
-          <Link href={"/login"}>
-             <Text style={styles.textLink}>Iniciar Sesión</Text>
-          </Link>            
-
+      <Link href={"/login"}>
+        <Text style={styles.textLink}>Iniciar Sesión</Text>
+      </Link>
     </View>
   );
-
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#ffffff', 
-    justifyContent: 'center',
+    backgroundColor: "#ffffff",
+    justifyContent: "center",
     padding: 20,
   },
   titleContainer: {
-    color: '#37513f',
-    flexDirection: 'row',
-   textAlign: 'center',
-   fontSize: 30,
+    color: "#37513f",
+    flexDirection: "row",
+    textAlign: "center",
+    fontSize: 30,
     gap: 8,
-    fontWeight: 'bold'
+    fontWeight: "bold",
   },
   input: {
     borderWidth: 1,
-    borderColor: '#A8A8A9',
-    backgroundColor: '#f3f3f3', 
-    color: '#676767',
+    borderColor: "#A8A8A9",
+    backgroundColor: "#f3f3f3",
+    color: "#676767",
     marginBottom: 10,
     padding: 10,
     borderRadius: 5,
   },
   button: {
-    backgroundColor: '#4A3717',
+    backgroundColor: "#4A3717",
     padding: 15,
-    alignItems: 'center',
+    alignItems: "center",
     borderRadius: 5,
   },
   text: {
-    color: '#000',
+    color: "#000",
     marginBottom: 10,
-    textAlign: 'center',
+    textAlign: "center",
     fontSize: 15,
   },
   textButton: {
-    color: '#ffffff',
+    color: "#ffffff",
     marginBottom: 10,
     fontSize: 20,
   },
   textLink: {
-    color: '#4e6e58',
+    color: "#4e6e58",
     marginBottom: 10,
     fontSize: 15,
-    textAlign: 'center'
-  }
+    textAlign: "center",
+  },
 });
-
